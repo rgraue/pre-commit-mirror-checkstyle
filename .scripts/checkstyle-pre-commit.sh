54e6f75533c77a5eb6d06881e7db37a1282e65f6
rgraue <rgraue@outlook.com>
2023-06-16 14:32:18 -0700

    idek again

--- .scripts/checkstyle-pre-commit.sh
+++ .scripts/checkstyle-pre-commit.sh
@@ -3,5 +3,7 @@
 echo "Downloading checkstyle 10.12.0"
 curl -LJO https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.12.0/checkstyle-10.12.0-all.jar
 
+ls
+
 echo "running linting command"
-java -jar checkstyle-10.12.0-all.jar -c ../google_checks.xml ./
+java -jar checkstyle-10.12.0-all.jar -c google_checks.xml ./
