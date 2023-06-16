67fc92c2e6591d6f94b9d4c71ad9bb2b4d1df591
rgraue <rgraue@outlook.com>
2023-06-16 13:44:15 -0700

    init:

---
+++ .scripts/checkstyle-pre-commit.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+
+echo "Downloading checkstyle 10.12.0"
+curl -LJO https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.12.0/checkstyle-10.12.0-all.jar
+
+echo "running linting command"
+java -jar checkstyle-10.12.0-all.jar
